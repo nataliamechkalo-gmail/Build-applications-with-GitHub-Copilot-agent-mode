19f716df4d00efc70b4034097b614c1a0c47ae86
Nataliia <natalia.mechkalo@gmail.com>
2026-03-24 21:44:36 +0000

    commit 3

--- octofit-tracker/backend/octofit_tracker/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/octofit_tracker/settings.py
@@ -138,8 +138,8 @@ STATIC_URL = 'static/'
 # CORS settings
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_HEADERS = ['*']
-CORS_ALLOW_METHODS = ['*']
+CORS_ALLOW_HEADERS = True
+CORS_ALLOW_METHODS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
